aeb10957e626ab6bffbba3138248f639de34baed
SalaniLeo <leonardo07.salani@gmail.com>
2022-09-28 19:06:13 +0200

    first commit

---
+++ background.py
@@ -0,0 +1,41 @@
+import os
+import time
+import os.path
+
+imgN = 0
+path = "/home/leo/Pictures/wallpapers/mix/"
+darkmode = "picture-uri-dark "
+savefilePath = "/home/leo/.cache/background/"
+savefileName = "background.num"
+savefileLoc = savefilePath + savefileName
+
+num_files = len([f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))])
+
+
+if(os.path.isfile(savefileLoc)):
+    with open(savefileLoc) as f: imgN = int(f.read())
+
+def setWallpaper():
+    global imgN
+
+    if (imgN == num_files):
+        imgN = 1
+    else:
+        imgN += 1
+
+    filename = "img" + str(imgN) + ".*"
+    command = "gsettings set org.gnome.desktop.background " + darkmode + path + filename
+
+    os.system(command)
+
+    if not os.path.isdir(savefilePath):
+        os.mkdir(savefilePath)
+
+    with open(savefileLoc, "w") as file:
+        file.write(str(imgN))
+
+    print(imgN)
+
+while True:
+    setWallpaper()
+    time.sleep(300)

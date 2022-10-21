2aca409f985b3b45d9fad4adf4abf8c275de614b
SalaniLeo <leonardo07.salani@gmail.com>
2022-10-21 20:53:24 +0200

    update

--- background.py
+++ background.py
@@ -2,24 +2,31 @@ import os
 import time
 import os.path
 from pathlib import Path
-
+import json
+ 
 home = str(Path.home())
 imgN = 0
 path = home + "/Pictures/wallpapers/mix/"
 darkmode = "picture-uri-dark "
-savefilePath = home + "/.cache/background/"
-savefileName = "background.num"
+savefilePath = home + "/.cache/background"
+savefileName = "/background.json"
 savefileLoc = savefilePath + savefileName
-
 num_files = len([f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))])
 
-
+#reads json file
 if(os.path.isfile(savefileLoc)):
-    with open(savefileLoc) as f: imgN = int(f.read())
+    with open(savefileLoc, 'r') as jsonfile:
+        data = jsonfile.read()
 
+    jsoncontent = json.loads(data)
+
+    imgN = jsoncontent['img']
+
+#sets the wallpaper
 def setWallpaper():
     global imgN
 
+    ##checks if imgnumber is more than the wallpapers and if so resets to the first
     if (imgN == num_files):
         imgN = 1
     else:
@@ -28,16 +35,20 @@ def setWallpaper():
     filename = "img" + str(imgN) + ".*"
     command = "gsettings set org.gnome.desktop.background " + darkmode + path + filename
 
+    ##executes the command to set wallpaper
     os.system(command)
 
-    if not os.path.isdir(savefilePath):
-        os.mkdir(savefilePath)
-
+    ##writes wallpaper number to json file
+    json_data = '{"img": ' + str(imgN) + '}'
+    jsonFIle = json.dumps(json_data)
     with open(savefileLoc, "w") as file:
-        file.write(str(imgN))
+        file.write(str(json_data))
 
-    print(imgN)
+#Creates json folder if not existing
+if not os.path.isdir(savefilePath):
+    os.mkdir(savefilePath)
 
+#every tot minutes changes background
 while True:
     setWallpaper()
-    time.sleep(300)
+    time.sleep(2)
